2f8eeacf1bb15cf0c35904c369254d974e65de0d
stanley <panteleevstas@gmail.com>
2024-09-18 15:41:22 +0500

    206. Reverse Linked List

---
+++ reverse-linked-list/reverse-linked-list/src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static ListNode reverseList(ListNode head) {
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+
+        return curr;
+    }
+
+
+}
