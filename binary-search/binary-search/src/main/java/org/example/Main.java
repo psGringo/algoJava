459f0fda24c0c8bc433682f0b4bc8116b798d000
stanley <panteleevstas@gmail.com>
2024-09-30 09:26:00 +0500

    just a binary search in 10 minutes

---
+++ binary-search/binary-search/src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        int res1 = binarySearchRecursive(arr, 0, arr.length - 1, 2);
+        int res2 = binarySearchIterative(arr, 0, arr.length - 1, 2);
+    }
+
+    private static int binarySearchRecursive(int[] arr, int lo, int hi, int value) {
+        int mid = lo + (hi - lo) / 2;
+        if (arr[mid] == value) {
+            return mid;
+        } else if (arr[mid] < value) {
+            return binarySearchRecursive(arr, mid, hi, value);
+        } else if (arr[mid] > value) {
+            return binarySearchRecursive(arr, lo, mid, value);
+        }
+        return -1;
+    }
+
+    private static int binarySearchIterative(int[] arr, int lo, int hi, int value) {
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] == value) {
+                return mid;
+            } else if (arr[mid] < value) {
+                lo = mid + 1;
+            } else if (arr[mid] > value) {
+                hi = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
