f3faa5cf027bbd14dae02ffa0906995d58d094aa
stanley <panteleevstas@gmail.com>
2024-10-02 13:18:59 +0500

    328. Odd Even Linked List (TODO)

---
+++ remove-outermost-parentheses/remove-outermost-parentheses/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+/**
+ * https://leetcode.com/problems/remove-outermost-parentheses/description/
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public String removeOuterParentheses(String s) {
+    // TODO
+    }
+}
