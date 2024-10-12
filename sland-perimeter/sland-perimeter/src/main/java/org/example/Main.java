d1aef0c121699d6cf7c10f9826face16b2e66827
stanley <panteleevstas@gmail.com>
2024-10-12 13:58:25 +0500

    [BFS] 463. Island Perimeter, ToDo

---
+++ sland-perimeter/sland-perimeter/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+/**
+ * https://leetcode.com/problems/island-perimeter/description/
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public int islandPerimeter(int[][] grid) {
+      // TODO
+    }
+
+}
