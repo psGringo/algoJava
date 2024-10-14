4910682247e27eda0e33fc009da9c82d10114bb3
stanley <panteleevstas@gmail.com>
2024-10-14 16:45:10 +0500

    [TREE INORDER] 94. Binary Tree Inorder Traversal

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal/src/main/java/org/example/Main.java
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal/src/main/java/org/example/Main.java
@@ -1,17 +1,31 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        doInorderTraversal(root, res);
+        return res;
+    }
+
+    public void doInorderTraversal(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        doInorderTraversal(node.left, list);
+        list.add(node.val);
+        doInorderTraversal(node.right, list);
+    }
+
+
 }
