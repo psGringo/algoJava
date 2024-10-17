5e3824b86cbea153824649fa6588a7c4aa165c8d
stanley <panteleevstas@gmail.com>
2024-10-17 21:26:08 +0500

    [BINARYSEARCH] 704. Binary Search

--- binary-search/binary-search/src/main/java/org/example/Main.java
+++ binary-search/binary-search/src/main/java/org/example/Main.java
@@ -3,34 +3,42 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 2, 3, 4, 5};
-        int res1 = binarySearchRecursive(arr, 0, arr.length - 1, 2);
-        int res2 = binarySearchIterative(arr, 0, arr.length - 1, 2);
+
     }
 
-    private static int binarySearchRecursive(int[] arr, int lo, int hi, int value) {
+    public int search(int[] nums, int target) {
+        return doSearchRecursive(nums, target, 0, nums.length - 1);
+    }
+
+    private int doSearchRecursive(int[] nums, int target, int lo, int hi) {
+        if (lo > hi) {
+            return -1;
+        }
         int mid = lo + (hi - lo) / 2;
-        if (arr[mid] == value) {
+        if (nums[mid] == target) {
             return mid;
-        } else if (arr[mid] < value) {
-            return binarySearchRecursive(arr, mid + 1, hi, value);
-        } else if (arr[mid] > value) {
-            return binarySearchRecursive(arr, lo, mid - 1, value);
+        } else if (nums[mid] > target) {
+            return doSearchRecursive(nums, target, lo, mid - 1);
+        } else if (nums[mid] < target) {
+            return doSearchRecursive(nums, target, mid + 1, hi);
         }
         return -1;
     }
 
-    private static int binarySearchIterative(int[] arr, int lo, int hi, int value) {
+    private int doSearchIterative(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-            if (arr[mid] == value) {
+            if (nums[mid] == target) {
                 return mid;
-            } else if (arr[mid] < value) {
-                lo = mid + 1;
-            } else if (arr[mid] > value) {
+            } else if (nums[mid] > target) {
                 hi = mid - 1;
+            } else if (nums[mid] < target) {
+                lo = mid + 1;
             }
         }
         return -1;
     }
+
 }
