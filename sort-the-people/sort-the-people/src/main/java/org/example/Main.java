c87204960f90a6e0b275d25376d0fdd90dddb3cb
stanley <panteleevstas@gmail.com>
2024-10-17 16:13:11 +0500

    [SORT] 3162. Find the Number of Good Pairs I

--- sort-the-people/sort-the-people/src/main/java/org/example/Main.java
+++ sort-the-people/sort-the-people/src/main/java/org/example/Main.java
@@ -1,6 +1,11 @@
 package org.example;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/sort-the-people/description/
  */
@@ -8,4 +13,29 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public String[] sortPeople(String[] names, int[] heights) {
+        Person[] people = new Person[heights.length];
+        for (int i = 0; i < names.length; i++) {
+            people[i] = new Person(names[i], heights[i]);
+        }
+        Arrays.sort(people, (x, y) -> {
+            if (x.height() > y.height()) {
+                return -1;
+            } else if (x.height() < y.height()) {
+                return 1;
+            }
+            return 0;
+        });
+
+        List<String> namesList = new ArrayList<>();
+        for (int i = 0; i < people.length; i++) {
+            namesList.add(people[i].name());
+        }
+        return namesList.toArray(new String[namesList.size()]);
+    }
+
+}
+
+record Person(String name, int height) {
 }
