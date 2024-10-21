fe561cca0572d44bf68f1986fae0386388c7327a
stanley <panteleevstas@gmail.com>
2024-10-21 15:08:18 +0500

    [BINARYSEARCH] 704. Binary Search https://junhaow.com/lc/notes/binary-search-template-notes.html

--- binary-search/binary-search/src/main/java/org/example/Main.java
+++ binary-search/binary-search/src/main/java/org/example/Main.java
@@ -3,12 +3,32 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        search(new int[]{1, 3, 4, 5, 6, 7, 8, 9, 10}, 3);
     }
 
-    public int search(int[] nums, int target) {
-        return doSearchRecursive(nums, target, 0, nums.length - 1);
+    public static int search(int[] nums, int target) {
+        int res = binarySearch(nums, target, 0, nums.length - 1);
+
+          return res;
     }
+
+    private static int binarySearch(int[] nums, int target, int lo, int hi) {
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] >= target) {
+                hi = mid - 1;  // for [lo, hi] || hi = mid for [lo, hi)
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        if (lo < nums.length && lo > 0 && nums[lo] == target) {
+            return lo;
+        }
+
+        return -1; // Returns <hi + 1> if not found
+    }
+
 
     private int doSearchRecursive(int[] nums, int target, int lo, int hi) {
         if (lo > hi) {
