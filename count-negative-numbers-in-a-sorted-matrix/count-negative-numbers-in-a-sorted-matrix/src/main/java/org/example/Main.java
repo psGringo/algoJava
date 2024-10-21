822571aaaadbff8294f55bce923b007583af3184
stanley <panteleevstas@gmail.com>
2024-10-21 20:58:52 +0500

    [BINARY SEARCH] 1351. Count Negative Numbers in a Sorted Matrix

--- count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix/src/main/java/org/example/Main.java
+++ count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix/src/main/java/org/example/Main.java
@@ -6,34 +6,52 @@ package org.example;
  */
 public class Main {
     public static void main(String[] args) {
+        int[][] grid = new int[9][9];
+        grid[0] = new int[]{8, -2, -2, -2, -4, -5, -5};
+        grid[1] = new int[]{-2, -3, -3, -4, -4, -5, -5};
+        grid[2] = new int[]{-2, -5, -5, -5, -5, -5, -5};
+        grid[3] = new int[]{-3, -5, -5, -5, -5, -5, -5};
+        grid[4] = new int[]{-5, -5, -5, -5, -5, -5, -5};
+        grid[5] = new int[]{-5, -5, -5, -5, -5, -5, -5};
+        grid[6] = new int[]{-5, -5, -5, -5, -5, -5, -5};
+        grid[7] = new int[]{-5, -5, -5, -5, -5, -5, -5};
+        grid[8] = new int[]{-5, -5, -5, -5, -5, -5, -5};
 
+        countNegatives(grid);
     }
+
     // TODO Додебажить, по каждому ряду ищем бинарным поиском первое отрицательное число
-    public int countNegatives(int[][] grid) {
+    public static int countNegatives(int[][] grid) {
         int count = 0;
         for (int i = 0; i < grid.length; i++) {
             int[] row = grid[i];
             int firstIndex = getFirstNegativeBinary(row, 0, row.length - 1);
             if (firstIndex >= 0) {
-                count = row.length - firstIndex;
+                count += row.length - firstIndex;
             }
         }
         return count;
     }
 
-    private int getFirstNegativeBinary(int[] row, int lo, int hi) {
-        if (hi < lo) {
-            return -1;
+    private static boolean g(int[] nums, int numIndex) {
+        return nums[numIndex] < 0;
+    }
+
+    private static int getFirstNegativeBinary(int[] nums, int lo, int hi) {
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (g(nums, mid)) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
         }
-        int mid = lo + (hi - lo) / 2;
-        System.out.println(mid - 1);
-        if (row[mid] < 0 && row[mid - 1] > 0) {
-            return mid;
-        } else if (row[mid] > 0 && row[mid - 1] > 0) {
-            return getFirstNegativeBinary(row, mid + 1, hi);
-        } else if (row[mid] < 0 && row[mid - 1] < 0) {
-            return getFirstNegativeBinary(row, lo, mid - 1);
+
+        if (lo < nums.length && lo >= 0 && nums[lo] < 0) {
+            return lo;
         }
+
         return -1;
     }
 }
