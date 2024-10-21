9c2c04266fbfc7b75295cc6d6aa0d8af956feed3
stanley <panteleevstas@gmail.com>
2024-10-21 17:24:42 +0500

    [ARRAY] 2859. Sum of Values at Indices With K Set Bits

--- sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits/src/main/java/org/example/Main.java
+++ sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits/src/main/java/org/example/Main.java
@@ -1,17 +1,41 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/sum-of-values-at-indices-with-k-set-bits/
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+     sumIndicesWithKSetBits(List.of(5,10,1,5,2), 1);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+          List<Integer> indices = new ArrayList<>();
+          for (int i = 0; i < nums.size(); i++) {
+              addIndexIfNeeded(k, i, indices, i);
+          }
+          int sum = 0;
+          for (int i = 0; i < indices.size(); i++) {
+              sum += nums.get(indices.get(i));
+          }
+          return sum;
+    }
+
+    private static void addIndexIfNeeded(int k, int num, List<Integer> indices, int i) {
+        int count = 0;
+        String binaryString = Integer.toBinaryString(num);
+        for (int j = 0; j < binaryString.length(); j++) {
+            if (binaryString.charAt(j) == '1') {
+                count++;
+
+            }
+        }
+        if (count == k) {
+            indices.add(i);
         }
     }
+
 }
