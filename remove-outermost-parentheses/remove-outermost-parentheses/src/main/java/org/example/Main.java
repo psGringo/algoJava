776ea9041f6e2ae4497cb566cc9f399f109242d6
stanley <panteleevstas@gmail.com>
2024-10-22 14:26:25 +0500

    [ARRAY] 1021. Remove Outermost Parentheses

--- remove-outermost-parentheses/remove-outermost-parentheses/src/main/java/org/example/Main.java
+++ remove-outermost-parentheses/remove-outermost-parentheses/src/main/java/org/example/Main.java
@@ -1,15 +1,40 @@
 package org.example;
 
 
+import java.util.*;
+
 /**
  * https://leetcode.com/problems/remove-outermost-parentheses/description/
  */
 public class Main {
     public static void main(String[] args) {
-
+        removeOuterParentheses(new String("(()())(())"));
     }
 
-    public String removeOuterParentheses(String s) {
-    // TODO
+    public static String removeOuterParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        Set<Integer> indexesToDelete = new HashSet<>();
+        int leftIndex = 0;
+        for (int i = 0; i < s.length(); i++) {
+            stack.push(s.charAt(i));
+            if (stack.peek() == ')') {
+                stack.pop();
+                stack.pop();
+                if (stack.isEmpty()) {
+                    indexesToDelete.add(leftIndex);
+                    indexesToDelete.add(i);
+                    if (i < s.length() - 1) {
+                        leftIndex = i + 1;
+                    }
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (!indexesToDelete.contains(i)) {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 }
