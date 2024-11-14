2fe64815ae633fa3bff5ad1dc1f61dda2b6db595
stanley <panteleevstas@gmail.com>
2024-11-14 16:20:29 +0500

    [BINARY SEARCH] 1608. Special Array With X Elements Greater Than or Equal X

--- special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x/src/main/java/org/example/Main.java
+++ special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x/src/main/java/org/example/Main.java
@@ -8,41 +8,49 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
-
+        specialArray(new int[]{3, 6, 7, 7, 0});
     }
 
-    public int specialArray(int[] nums) {
-        Arrays.sort(nums);
-        return binarySearch(nums, 0, nums.length - 1);
+    public static int specialArray(int[] nums) {
+        return binarySearch(nums);
     }
 
-    private int count(int[] nums, int num) {
+    private static int count(int[] nums, int mid) {
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= num) {
+            if (nums[i] >= mid) {
                 count++;
             }
         }
         return count;
     }
 
-    private int binarySearch(int[] nums, int lo, int hi) {
+    /**
+     * Sometimes we need exact count
+     *
+     * @param nums
+     * @return
+     */
+    private static int binarySearch(int[] nums) {
+        int lo = 0;
+        int max = Arrays.stream(nums).max().getAsInt();
+        int hi = max;
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
             int count = count(nums, mid);
-            if (count <= mid) {
+
+            if (mid == count) {
+                return mid;
+            }
+
+            if (mid > count) {
                 hi = mid - 1;  // for [lo, hi] || hi = mid for [lo, hi)
             } else {
                 lo = mid + 1;
             }
         }
 
-        if (lo > 0 && lo < nums.length) {
-            return lo;
-        }
-
         return -1; // Returns <hi + 1> if not found
     }
-
 
 }
