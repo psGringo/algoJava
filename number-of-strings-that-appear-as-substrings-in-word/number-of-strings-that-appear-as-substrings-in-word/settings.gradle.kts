f600614dcdd37b24afc51258cc0ad8a2e45785d0
stanley <panteleevstas@gmail.com>
2024-11-17 02:10:19 +0500

    [STRINGS] 1967. Number of Strings That Appear as Substrings in Word

---
+++ number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word/settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "number-of-strings-that-appear-as-substrings-in-word"
+
---
+++ number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public int numOfStrings(String[] patterns, String word) {
+        int count = 0;
+        for (int i = 0; i < patterns.length; i++) {
+            if (word.contains(patterns[i])) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
