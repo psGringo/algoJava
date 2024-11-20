b39af906196e2352e6a8d9c21dcefe087599427c
stanley <panteleevstas@gmail.com>
2024-11-21 04:24:13 +0500

    [STRING] 1002. Find Common Characters, not very easy )

--- find-common-characters/find-common-characters/src/main/java/org/example/Main.java
+++ find-common-characters/find-common-characters/src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -15,7 +16,7 @@ public class Main {
 
     public static List<String> commonChars(String[] words) {
 
-        // what chars did we meet?
+        // what chars did we meet and with what frequency?
         List<HashMap<Character, Integer>> list = new ArrayList<>();
         for (String word : words) {
             HashMap<Character, Integer> map = new HashMap<>();
@@ -25,20 +26,20 @@ public class Main {
             list.add(map);
         }
 
-        // TODO: optimize, iterate only by letters met, not all alphabet...
+        // TODO: Optimize with min, max chars we met
         List<String> result = new ArrayList<>();
         for (int letter = 'A'; letter <= 'z'; letter++) {
             boolean isEveryHashmapContainsLetter = true;
             while (isEveryHashmapContainsLetter) {
                 for (HashMap<Character, Integer> map : list) {
-                    if (!map.containsKey((char)letter) || (map.get((char)letter) == 0)) {
+                    if (!map.containsKey((char) letter) || (map.get((char) letter) == 0)) {
                         isEveryHashmapContainsLetter = false;
                     }
                 }
                 if (isEveryHashmapContainsLetter) {
-                    result.add(String.valueOf((char)letter));
+                    result.add(String.valueOf((char) letter));
                     for (HashMap<Character, Integer> map : list) {
-                        int countLetters = map.get((char)letter);
+                        int countLetters = map.get((char) letter);
                         int newCountLetters = 0;
                         if (countLetters >= 1) {
                             newCountLetters = countLetters - 1;
@@ -48,7 +49,35 @@ public class Main {
                 }
             }
         }
-
         return result;
+
+//        Doesn't work well with sets
+//        List<String> result = new ArrayList<>();
+//        var iterator = allCharsWeMet.iterator();
+//        while (iterator.hasNext()) {
+//            Character letter = iterator.next();
+//
+//            boolean isEveryHashmapContainsLetter = true;
+//            while (isEveryHashmapContainsLetter) {
+//                for (HashMap<Character, Integer> map : list) {
+//                    if (!map.containsKey((char)letter) || (map.get((char)letter) == 0)) {
+//                        isEveryHashmapContainsLetter = false;
+//                    }
+//                }
+//                if (isEveryHashmapContainsLetter) {
+//                    result.add(String.valueOf((char)letter));
+//                    for (HashMap<Character, Integer> map : list) {
+//                        int countLetters = map.get((char)letter);
+//                        int newCountLetters = 0;
+//                        if (countLetters >= 1) {
+//                            newCountLetters = countLetters - 1;
+//                        }
+//                        map.put((char) letter, newCountLetters);
+//                    }
+//                }
+//            }
+//        }
+
+
     }
 }
