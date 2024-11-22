36e166005b58d791452034530f97f2731381406d
stanley <panteleevstas@gmail.com>
2024-11-22 21:32:14 +0500

    [STRING] 1370. Increasing Decreasing String

--- increasing-decreasing-string/increasing-decreasing-string/src/main/java/org/example/Main.java
+++ increasing-decreasing-string/increasing-decreasing-string/src/main/java/org/example/Main.java
@@ -1,15 +1,38 @@
 package org.example;
 
 
-/**
- * https://leetcode.com/problems/increasing-decreasing-string/
- */
 public class Main {
     public static void main(String[] args) {
-
+        sortString("aaaabbbbcccc");
     }
 
-    public String sortString(String s) {
-         // TODO..., skipped 'cause of unclear description
+    public static String sortString(String s) {
+        int[] fq = new int[131];
+        for (int i = 0; i < s.length(); i++) {
+            fq[(int) s.charAt(i)]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            boolean needBreak = true;
+            for (int i = 65; i < 130; i++) {
+                if (fq[i] > 0) {
+                    sb.append((char) i);
+                    fq[i]--;
+                    needBreak = false;
+                }
+            }
+            for (int i = 130; i >= 65; i--) {
+                if (fq[i] > 0) {
+                    sb.append((char) i);
+                    fq[i]--;
+                    needBreak = false;
+                }
+            }
+            if (needBreak) {
+                break;
+            }
+        }
+        return sb.toString();
     }
 }
