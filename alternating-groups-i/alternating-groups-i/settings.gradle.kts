afa26bcda7b2ba3f429c461d3d89312d6ce9ccc9
stanley <panteleevstas@gmail.com>
2024-12-05 02:02:51 +0500

    [SLIDING WINDOW] 3206. Alternating Groups I

---
+++ alternating-groups-i/alternating-groups-i/settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "alternating-groups-i"
+
---
+++ alternating-groups-i/alternating-groups-i/src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        numberOfAlternatingGroups(new int[]{0, 1, 0, 0, 1});
+    }
+
+    public static int numberOfAlternatingGroups(int[] colors) {
+        int n = colors.length;
+        int[] doubleColours = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            doubleColours[i] = colors[i];
+            doubleColours[i + n] = colors[i];
+        }
+        int start = 0;
+        int i = start;
+        int j = i + 2;
+        int res = 0;
+        while (true) {
+            if (doubleColours[i] == doubleColours[j] && doubleColours[i] != doubleColours[i + 1]) {
+                res++;
+            }
+            i++;
+            j++;
+            if (i == start + n) {
+                break;
+            }
+        }
+        return res;
+    }
+}
