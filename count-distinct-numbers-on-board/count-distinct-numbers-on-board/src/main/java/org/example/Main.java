53303fe530ad50d693301310a06def8c9e2703c8
stanley <panteleevstas@gmail.com>
2024-12-26 16:34:09 +0500

    [ARRAYS] 2549. Count Distinct Numbers on Board

---
+++ count-distinct-numbers-on-board/count-distinct-numbers-on-board/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public int distinctIntegers(int n) {
+        return n == 1 ? 1 : n - 1;
+    }
+}
