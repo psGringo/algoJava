8887a03de9c6a6505909bf7c046f3148d83b695c
stanley <panteleevstas@gmail.com>
2025-01-05 18:59:55 +0500

    [STRINGS] 696. Count Binary Substrings

--- count-binary-substrings/count-binary-substrings/src/main/java/org/example/Main.java
+++ count-binary-substrings/count-binary-substrings/src/main/java/org/example/Main.java
@@ -1,29 +1,40 @@
 package org.example;
 
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        numberOfSpecialChars("dDDDd");
+        countBinarySubstrings("00110011");
     }
 
-    public static int numberOfSpecialChars(String word) {
-        HashSet<Character> set = new HashSet<>();
 
-        for (char c : word.toCharArray()) {
-            set.add(c);
-        }
+    public static int countBinarySubstrings(String s) {
+        // List to store the lengths of consecutive runs
+        List<Integer> groupLengths = new ArrayList<>();
+        int count = 1;
 
-        int count = 0;
-        int diff = Math.abs('a' - 'A');
-        for (int i = 0; i <= 26; i++) {
-            char c = (char) ((int) 'a' + i);
-            if (set.contains(c) && set.contains((char) ((int) c - diff))) {
+        // Traverse the string to calculate lengths of consecutive runs
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
                 count++;
+            } else {
+                groupLengths.add(count);
+                count = 1;
             }
         }
+        // Add the last group length
+        groupLengths.add(count);
 
-        return count;
+        // Count valid substrings
+        int result = 0;
+        for (int i = 1; i < groupLengths.size(); i++) {
+            result += Math.min(groupLengths.get(i - 1), groupLengths.get(i));
+        }
+
+        return result;
     }
+
+
 }
