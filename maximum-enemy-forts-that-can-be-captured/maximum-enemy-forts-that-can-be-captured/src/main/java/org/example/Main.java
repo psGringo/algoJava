0a2f3b3ccc39d67a4f30d0511c752023a5ca0343
stanley <panteleevstas@gmail.com>
2025-01-13 10:22:05 +0500

    [ARRAYS] 2511. Maximum Enemy Forts That Can Be Captured

--- maximum-enemy-forts-that-can-be-captured/maximum-enemy-forts-that-can-be-captured/src/main/java/org/example/Main.java
+++ maximum-enemy-forts-that-can-be-captured/maximum-enemy-forts-that-can-be-captured/src/main/java/org/example/Main.java
@@ -1,29 +1,23 @@
 package org.example;
 
 
-import java.util.Arrays;
-
+/**
+ * https://leetcode.com/problems/maximum-enemy-forts-that-can-be-captured/description/
+ */
 public class Main {
     public static void main(String[] args) {
 
     }
 
-    public String triangleType(int[] nums) {
-        // Sort the array to simplify comparisons
-        Arrays.sort(nums);
-
-        // Check for triangle validity using the triangle inequality theorem
-        if (nums[0] + nums[1] <= nums[2]) {
-            return "none"; // Not a valid triangle
-        }
-
-        // Classify the triangle
-        if (nums[0] == nums[2]) {
-            return "equilateral"; // All sides are equal
-        } else if (nums[0] == nums[1] || nums[1] == nums[2]) {
-            return "isosceles"; // Exactly two sides are equal
-        } else {
-            return "scalene"; // All sides are different
-        }
+    public int captureForts(int[] forts) {
+        int ans = 0;
+        for (int i = 0, j = 0; i < forts.length; ++i)
+            if (forts[i] != 0) {
+                if (forts[j] !=0 && forts[i] != forts[j])
+                    ans = Math.max(ans, i - j - 1);
+                j = i;
+            }
+        
+        return ans;
     }
 }
